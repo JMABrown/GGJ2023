75affd9299b1cbe5f4d560a720abc9befd20bbaa
GameJammer <jbrown74@qub.ac.uk>
2023-02-05 16:14:24 +0000

    Fixed wandering behaviou

--- Assets/TroopBehaviour.cs
+++ Assets/TroopBehaviour.cs
@@ -9,8 +9,6 @@ public class TroopBehaviour : MonoBehaviour
 {
     private GenericGrid<Cell> _board;
 
-    private List<Cell.Direction> _directions;
-
     private bool _moving = false;
     
     private bool _dead = false;
@@ -22,12 +20,6 @@ public class TroopBehaviour : MonoBehaviour
     
     void Start()
     {
-        _directions = new List<Cell.Direction>(4);
-        _directions.Add(Cell.Direction.Up);
-        _directions.Add(Cell.Direction.Down);
-        _directions.Add(Cell.Direction.Left);
-        _directions.Add(Cell.Direction.Right);
-
         _board = GameManager.Instance.GameBoard;
         _troopMovementRoutine = StartCoroutine(TroopMovementBrain());
     }
@@ -41,54 +33,72 @@ public class TroopBehaviour : MonoBehaviour
             _board = GameManager.Instance.GameBoard;
             
             var position = transform.position;
+            Debug.Log($"Position: {position}");
             var simplePosition = new Vector2(Mathf.Floor(position.x), Mathf.Floor(position.y));
-            var randomDirections = new List<Cell.Direction>(_directions.Count);
-            int originalDirectionsCount = Int32.Parse(_directions.Count.ToString()); // Some hack to break a strange reevaluation of this
-            for (int i = 0; i < originalDirectionsCount; i++)
-            {
-                var randIndex = Random.Range(0, _directions.Count);
-                randomDirections.Add(_directions[randIndex]);
-                _directions.RemoveAt(randIndex);
-            }
-            _directions = randomDirections;
-
+            Debug.Log($"SimplePosition: {simplePosition}");
+            
             Cell validWanderCell = null;
-            for (int i = 0; i < randomDirections.Count; i++)
+            var cellTrooperIsIn = _board[(int)simplePosition.y, (int)simplePosition.x];
+            Debug.Log($"cellTrooperIsIn: {cellTrooperIsIn}");
+            List<Cell> possibleWanderCells = new List<Cell>();
+
+            var neighbouringCell = cellTrooperIsIn.Neighbours[Cell.Direction.Up];
+            if (neighbouringCell != null)
             {
-                var selectedRandomDirection = randomDirections[i];
-                var cellTrooperIsIn = _board[(int)simplePosition.y, (int)simplePosition.x];
-                var wanderCell = cellTrooperIsIn.Neighbours[selectedRandomDirection];
-                if (wanderCell == null)
-                {
-                    continue;
-                }
-                if (wanderCell.PlantLevel == 0)
-                {
-                    validWanderCell = wanderCell;
-                    break;
-                }
+                Debug.Log("Adding wander cell: UP");
+                possibleWanderCells.Add(neighbouringCell);
+            }
+            neighbouringCell = cellTrooperIsIn.Neighbours[Cell.Direction.Down];
+            if (neighbouringCell != null)
+            {
+                Debug.Log("Adding wander cell: DOWN");
+                possibleWanderCells.Add(neighbouringCell);
+            }
+            neighbouringCell = cellTrooperIsIn.Neighbours[Cell.Direction.Left];
+            if (neighbouringCell != null)
+            {
+                Debug.Log("Adding wander cell: LEFT");
+                possibleWanderCells.Add(neighbouringCell);
+            }
+            neighbouringCell = cellTrooperIsIn.Neighbours[Cell.Direction.Right];
+            if (neighbouringCell != null)
+            {
+                Debug.Log("Adding wander cell: RIGHT");
+                possibleWanderCells.Add(neighbouringCell);
             }
 
-            if (validWanderCell == null)
+            var randomChoice = Random.Range(0, possibleWanderCells.Count);
+            var randomChoiceCell = possibleWanderCells[randomChoice];
+            Debug.Log($"Possible wander cells: {possibleWanderCells}");
+            Debug.Log($"Random choice: {randomChoice}");
+
+            if (randomChoiceCell.PlantLevel != 0)
             {
-                continue;
+                Debug.Log("Random choice has plant level");
+                break;
             }
             
-            var wanderCellPosition = validWanderCell.transform.position;
-            _wanderPosition = new Vector3(wanderCellPosition.x + Random.insideUnitCircle.x*0.2f,
-                wanderCellPosition.y + Random.insideUnitCircle.y*0.2f,
+            var wanderCellPosition = randomChoiceCell.transform.position;
+            //var randomNoise = Random.insideUnitCircle;
+            var randomNoise = Vector2.zero;
+            _wanderPosition = new Vector3(wanderCellPosition.x + randomNoise.x*0.2f,
+                wanderCellPosition.y + randomNoise.y*0.2f,
                 0f);
+            Debug.Log($"New _wanderPosition: {wanderCellPosition}");
             
             _moving = true;
+            Debug.Log($"Starting move...");
             DOTween.To(() => transform.position, x => transform.position = x, _wanderPosition, Globals.WanderTime)
                 .SetEase(Ease.Linear).onComplete += OnMoveComplete;
 
             yield return new WaitUntil(() => !_moving);
+            
         }
     }
 
     private void OnMoveComplete()
     {
         _moving = false;
+        Debug.Log($"Move complete");
     }
 }
